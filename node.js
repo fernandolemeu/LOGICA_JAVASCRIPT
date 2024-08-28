ab1cd74ecaf9a0525f1cf24562e7486d7f4fe0c6
fernandolemeu <165900357+fernandolemeu@users.noreply.github.com>
2024-08-28 08:00:12 -0300

    node.js

---
+++ node.js
@@ -0,0 +1,29 @@
+var nome ="John";
+let idade = 25;
+const pi = 3.14;
+
+let a = 5;
+let b = 10;
+let soma = a+b; // armazena o resultado da soma de a e b na variavel sum
+console.log(soma); // 15
+
+let a = 5;
+let b = 10;
+
+function multiplica (x, y) {
+    return x * y;
+}
+
+let produto = multiplica (a, b); // armazena o resultado da função multiplica na variavel produto
+console.log(produto); // 50
+
+let frutas = ['apple','banana', 'cherry'];
+console.log(frutas[1]); //banana
+
+let pessoa = {
+    primeironome: 'John',
+    sobrenome: 'Delon',
+    age: 25
+};
+
+console.log (pessoa.primeironome); // John
